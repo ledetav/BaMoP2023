556c044e2b5bc9f3a8cf9c96bd30371d5ea84707
ledetav <nord.hilman@gmail.com>
2023-03-09 00:16:33 +0300

    add Files 1.0 middle task

---
+++ homeworks/Middle tasks/Files 1.0 [middle]/files1.py
@@ -0,0 +1,13 @@
+import os
+
+def print_docs(directory):
+    for root, dirs, files in os.walk(directory):
+        level = root.replace(directory, '').count(os.sep)
+        indent = ' ' * 4 * level
+        print(f"{indent}{os.path.basename(root)}/")
+        subindent = ' ' * 4 * (level + 1)
+        for file in files:
+            print(f"{subindent}{file}")
+
+directory = 'R:\edu\BaMoP2023\homeworks'
+print_docs(directory)
