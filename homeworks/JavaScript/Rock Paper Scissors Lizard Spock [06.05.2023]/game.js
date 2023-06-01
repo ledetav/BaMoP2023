d00b6e11d1d038942a6c3be1f0415a5f6eecdc5b
ledetav <nord.hilman@gmail.com>
2023-06-01 23:57:02 +0300

    update Rock Paper Scissors Lizard Spock [JS]

--- homeworks/JavaScript/Rock Paper Scissors Lizard Spock [06.05.2023]/game.js
+++ homeworks/JavaScript/Rock Paper Scissors Lizard Spock [06.05.2023]/game.js
@@ -1,3 +1,13 @@
+const score = {
+    player: 0,
+    computer: 0,
+    draws: 0
+};
+
+function updateScoreDisplay() {
+    document.getElementById('score').innerText = `Player: ${score.player} | Computer: ${score.computer} | Draws: ${score.draws}`;
+}
+
 function getRandomChoice() {
     const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     return choices[Math.floor(Math.random() * choices.length)];
@@ -21,6 +31,19 @@ function determineWinner(playerChoice, computerChoice) {
     }
 }
 
+function saveScoreToJson() {
+    const scoreJson = JSON.stringify(score);
+    const blob = new Blob([scoreJson], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'score.json';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+}
+
 function playGame(playerChoice) {
     const computerChoice = getRandomChoice();
     const winner = determineWinner(playerChoice, computerChoice);
@@ -28,11 +51,18 @@ function playGame(playerChoice) {
 
     if (winner === 'draw') {
         resultText = `It's a draw! You both chose ${playerChoice}.`;
+        score.draws++;
     } else if (winner === 'player') {
         resultText = `You win! ${playerChoice} beats ${computerChoice}.`;
+        score.player++;
     } else {
         resultText = `Computer wins! ${computerChoice} beats ${playerChoice}.`;
+        score.computer++;
     }
 
     document.getElementById('result').innerText = resultText;
+    updateScoreDisplay();
 }
+
+const saveButton = document.getElementById('save-button');
+saveButton.addEventListener('click', saveScoreToJson);
